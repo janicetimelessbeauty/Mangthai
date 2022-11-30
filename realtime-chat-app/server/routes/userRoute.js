70b6912442d2bc21e2de0b025a63527fccf12438
QUEEN NGA <98411934+janicetimelessbeauty@users.noreply.github.com>
2022-11-30 16:15:55 +1100

    Delete realtime-chat-app directory

--- realtime-chat-app/server/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const {register, login, setAvatar, getAllusers} = require("../controllers/userController")
-const router = require("express").Router()
-router.post("/register", register )
-router.post("/login", login )
-router.post("/setavatar/:id", setAvatar)
-router.get("/allusers/:id", getAllusers)
-module.exports = router
